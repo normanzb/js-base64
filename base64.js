a3b8648a883367bd2fed4b32e5809cad745ddc88
Norman Xu <im@norm.im>
2016-05-05 17:48:10 +0100

    remove extension of string object, cus that is bad practise remove built in support for meteor for cohesive

--- base64.js
+++ base64.js
@@ -167,28 +167,4 @@
         decode: decode,
         noConflict: noConflict
     };
-    // if ES5 is available, make Base64.extendString() available
-    if (typeof Object.defineProperty === 'function') {
-        var noEnum = function(v){
-            return {value:v,enumerable:false,writable:true,configurable:true};
-        };
-        global.Base64.extendString = function () {
-            Object.defineProperty(
-                String.prototype, 'fromBase64', noEnum(function () {
-                    return decode(this)
-                }));
-            Object.defineProperty(
-                String.prototype, 'toBase64', noEnum(function (urisafe) {
-                    return encode(this, urisafe)
-                }));
-            Object.defineProperty(
-                String.prototype, 'toBase64URI', noEnum(function () {
-                    return encode(this, true)
-                }));
-        };
-    }
-    // that's it!
-    if (global['Meteor']) {
-       Base64 = global.Base64; // for normal export in Meteor.js
-    }
 })(this);
